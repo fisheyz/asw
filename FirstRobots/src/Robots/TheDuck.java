9b43f4634e4c6d23828470c09549911f22971a08
Fishey <tiago_m4t3us@hotmail.com>
2017-02-16 13:52:50 +0000

    fc

---
+++ FirstRobots/src/Robots/TheDuck.java
@@ -0,0 +1,17 @@
+package Robots;
+import robocode.*;
+  
+ public class TheDuck extends Robot {
+     public void run() {
+         while (true) {
+             ahead(100);
+             turnGunRight(360);
+             back(100);
+             turnGunRight(360);
+         }
+     }
+  
+     public void onScannedRobot(ScannedRobotEvent e) {
+         fire(1);
+     }
+ }
